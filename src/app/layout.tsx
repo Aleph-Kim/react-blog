22d441b1102914d085dc0ba5de24f2f44ae3e780
김채민 <dktjdej25@gmail.com>
2024-10-14 13:56:36 +0900

    스타일 - 기본 레이아웃 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css";
+import Link from "next/link";
+
+export const metadata: { title: string } = {
+    title: 'react blog'
+}
 
 export default function RootLayout({
     children,
@@ -8,7 +13,13 @@ export default function RootLayout({
     return (
         <html lang="ko" data-theme="dark">
             <body>
-                {children}
+                <div className="navbar bg-base-100 flex justify-between">
+                    <Link href="/" className="btn btn-ghost text-xl">홈</Link>
+                    <Link href="/write" className="btn btn-ghost text-xl">글 작성</Link>
+                </div>
+                <div className="p-5">
+                    {children}
+                </div>
             </body>
         </html>
     );
