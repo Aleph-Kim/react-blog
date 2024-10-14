66233f91eb7ab06053772d9779b513b70cff9df8
김채민 <dktjdej25@gmail.com>
2024-10-14 13:59:44 +0900

    스타일 - 게시글 목록 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-export default function Home() {
+type Post = {
+    id: number;
+    title: string;
+};
+const posts: Post[] = [
+    { id: 1, title: '1번 게시글' },
+    { id: 2, title: '2번 게시글' },
+    { id: 3, title: '3번 게시글' },
+];
 
+export default function Home() {
     return (
-        <div className="font-extrabold">
-            Hello World!
+        <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold mb-6">게시글 목록</h1>
+            <ul className="space-y-4">
+                {posts.map((post) => (
+                    <li
+                        key={post.id}
+                        className="rounded-md p-4 cursor-pointer bg-[#303030] transform transition-transform duration-300 hover:-translate-y-2"
+                    >
+                        <h2 className="text-xl font-semibold text-white">{post.title}</h2>
+                    </li>
+                ))}
+            </ul>
         </div>
+
     );
 }
