47dd261904fbf36e346d98cc6e23e825c5dc50a9
김채민 <dktjdej25@gmail.com>
2024-10-18 17:48:42 +0900

    새 기능 - 게시글 목록 출력 기능

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,23 @@
-type Post = {
-    id: number;
-    title: string;
-};
-const posts: Post[] = [
-    { id: 1, title: '1번 게시글' },
-    { id: 2, title: '2번 게시글' },
-    { id: 3, title: '3번 게시글' },
-];
+import { supabase } from "@/lib/supabaseClient";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+    const { data: posts } = await supabase.from('post').select('*');
+
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-3xl font-bold mb-6">게시글 목록</h1>
-            <ul className="space-y-4">
-                {posts.map((post) => (
-                    <li
+            <div className="space-y-4">
+                {posts?.map((post) => (
+                    <Link
                         key={post.id}
-                        className="rounded-md p-4 cursor-pointer bg-[#303030] transform transition-transform duration-300 hover:-translate-y-2"
+                        href={`/detail/${post.id}`}
+                        className="block rounded-md p-4 cursor-pointer bg-[#303030] transform transition-transform duration-300 hover:-translate-y-2"
                     >
-                        <h2 className="text-xl font-semibold text-white">{post.title}</h2>
-                    </li>
+                        <h3 className="text-xl font-semibold text-white">{post.title}</h3>
+                    </Link>
                 ))}
-            </ul>
+            </div>
         </div>
-
     );
 }
